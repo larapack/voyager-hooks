95525fbf83b830a3915e2518120395563e3b78ae
Mark Topper <mark@ulties.com>
2018-01-17 08:55:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- publishable/config/voyager-hooks.php
+++ publishable/config/voyager-hooks.php
@@ -2,11 +2,11 @@
 
 return [
 
-	'enabled' => env('HOOKS_ENABLED', true),
+    'enabled' => env('HOOKS_ENABLED', true),
 
-	'add-route' => true,
-	'add-hook-menu-item' => true,
-	'add-hook-permissions' => true,
-	'publish-vendor-files' => true,
+    'add-route'            => true,
+    'add-hook-menu-item'   => true,
+    'add-hook-permissions' => true,
+    'publish-vendor-files' => true,
 
 ];
